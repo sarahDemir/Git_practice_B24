8620a8e0c4aa2a9e3d91374b1c21f506af87a868
sarahDemir <sarahseymademir@gmail.com>
2021-10-03 14:33:33 -0400

    added print

--- src/Setup/Third.java
+++ src/Setup/Third.java
@@ -4,6 +4,6 @@ public class Third {
     //some new code
     //this is next line
     public static void main(String[] args) {
-        System.out.println();
+        System.out.println("5");
     }
 }
